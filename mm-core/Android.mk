a80f96521970dadf2b56544bfcb607ebb0aa2daf
fourninesix <klmajor2213@gmail.com>
2024-11-06 08:16:52 +0000

    Revert "media: Use project pathmap"
    This reverts commit 05881a16d44b82554ede964176854dcf209f9bbb.

--- mm-core/Android.mk
+++ mm-core/Android.mk
@@ -35,7 +35,7 @@ endif
 #===============================================================================
 
 LOCAL_C_INCLUDES        := $(LOCAL_PATH)/src/common
-LOCAL_C_INCLUDES        += $(call project-path-for,qcom-media)/libplatformconfig
+LOCAL_C_INCLUDES        += $(TOP)/hardware/qcom/media/libplatformconfig
 
 LOCAL_HEADER_LIBRARIES := \
         libutils_headers \
@@ -70,7 +70,7 @@ include $(BUILD_SHARED_LIBRARY)
 include $(CLEAR_VARS)
 
 LOCAL_C_INCLUDES        := $(LOCAL_PATH)/src/common
-LOCAL_C_INCLUDES        += $(call project-path-for,qcom-media)/libplatformconfig
+LOCAL_C_INCLUDES        += $(TOP)/hardware/qcom/media/libplatformconfig
 
 LOCAL_HEADER_LIBRARIES := \
         libutils_headers \
